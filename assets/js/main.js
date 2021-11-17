63144af8e0112f1509283ebb3f0179f86434b24a
John Rojas <jrojas207@estudiantes.areandina.edu.co>
2021-11-17 00:56:53 -0500

    final version of the project without errors

--- assets/js/main.js
+++ assets/js/main.js
@@ -254,52 +254,37 @@ const role = document.querySelector('.role')
 
 const testimonials = [
   {
-    name: 'Gloria Rojas',
-    position: 'Franservis, Gerente',
+    name: 'Elizabeth Gutierrez',
+    position: 'Lawyer - Prochampions S.A.S.',
     photo:
       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=707b9c33066bf8808c934c8ab394dff6',
     text:
-      "I've worked with literally hundreds of HTML/CSS developers and I have to say the top spot goes to this guy. This guy is an amazing developer. He stresses on good, clean code and pays heed to the details. I love developers who respect each and every aspect of a throughly thought out design and do their best to put it in code. He goes over and beyond and transforms ART into PIXELS - without a glitch, every time.",
+      "I've worked with literally hundreds of HTML/CSS developers and I have to say the top spot goes to this guy. This guy is an amazing developer. He stresses on good, clean code and pays heed to the details. I love developers who respect each and every aspect of a throughly thought out design and do their best to put it in code.",
   },
   {
     name: 'Goku',
-    position: 'Protector of the earth',
+    position: 'Protector Of The Earth',
     photo: 'https://images.unsplash.com/photo-1606663889134-b1dedb5ed8b7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80',
     text:
       'I love this guy, is an amazing frontend developer that delivered the task exactly how we need it, do your self a favor and hire him, you will not be disappointed by the work delivered. He will go the extra mile to make sure that you are happy with your project. I will surely work again with him!',
   },
   {
-    name: 'Iida Niskanen',
-    position: 'Data Entry',
+    name: 'Andres aldana - Prochampions S.A.S.',
+    position: 'Retail Director',
     photo: 'https://randomuser.me/api/portraits/women/68.jpg',
     text:
       "This guy is a hard worker. Communication was also very good with him and he was very responsive all the time, something not easy to find in many freelancers. We'll definitely repeat with him.",
   },
   {
-    name: 'sdfsdfDA',
-    position: 'Receptionist',
-    photo: 'https://randomuser.me/api/portraits/women/65.jpg',
-    text:
-      "This guy does everything he can to get the job done and done right. This is the second time I've hired him, and I'll hire him again in the future.",
-  },
-  {
-    name: 'dfdsfsfsdf',
-    position: 'Graphic Designer',
+    name: 'Camilo Beltran',
+    position: 'Graphic Designer - Prochampions S.A.S.',
     photo: 'https://randomuser.me/api/portraits/men/43.jpg',
     text:
       "I had my concerns that due to a tight deadline this project can't be done. But this guy proved me wrong not only he delivered an outstanding work but he managed to deliver 1 day prior to the deadline. And when I asked for some revisions he made them in MINUTES. I'm looking forward to work with him again and I totally recommend him. Thanks again!",
   },
   {
-    name: 'dfsfsdf',
-    position: 'Accountant',
-    photo:
-      'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?h=350&auto=compress&cs=tinysrgb',
-    text:
-      'This guy is a top notch designer and front end developer. He communicates well, works fast and produces quality work. We have been lucky to work with him!',
-  },
-  {
-    name: 'José Franco',
-    position: 'Director',
+    name: 'Jose Franco',
+    position: 'Principal Director - Franservis',
     photo: 'https://randomuser.me/api/portraits/men/97.jpg',
     text:
       'This guy is a young and talented IT professional, proactive and responsible, with a strong work ethic. He is very strong in PSD2HTML conversions and HTML/CSS technology. He is a quick learner, eager to learn new technologies. He is focused and has the good dynamics to achieve due dates and outstanding results.',
@@ -322,7 +307,7 @@ function updateTestimonial() {
     idx = 0
   }
 }
-setInterval(updateTestimonial, 10000)
+setInterval(updateTestimonial, 10000);
 
 // audio
 const audio = document.getElementById("audio");
